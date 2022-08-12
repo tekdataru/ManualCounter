06c22ed9f21177d56da5e7a9f3a240c2a7de8c54
Nayanov Konstantin <8603@mail.ru>
2022-08-12 17:31:20 +0500

    2 without icon, but works!

--- app/src/main/java/ru/tekdata/manualcounter/MainActivity.kt
+++ app/src/main/java/ru/tekdata/manualcounter/MainActivity.kt
@@ -1,11 +1,45 @@
 package ru.tekdata.manualcounter
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings.Global.putString
+import ru.tekdata.manualcounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val prefs = getSharedPreferences("tekdata_ru", Context.MODE_PRIVATE)
+
+
+        prefs.getString("tekdata_manual_counter", "0")?.let { binding.editText.setText(it) }
+
+
+
+
+        binding.buttonUp.setOnClickListener {
+            val buf = binding.editText.text.toString().toInt()
+            binding.editText.setText((buf + 1).toString())
+
+
+                prefs.edit().apply{
+                    putString("tekdata_manual_counter", (buf + 1).toString())
+                    commit()
+
+                }
+
+        }
+
+        binding.buttonDown.setOnClickListener {
+            val buf = binding.editText.text.toString().toInt()
+            binding.editText.setText((buf - 1).toString())
+        }
+
     }
 }
